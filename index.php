ed94621af1c7e692925ff3de2ebf2acc3b0f2357
TheBoinas <106340856+TheBoinas@users.noreply.github.com>
2022-05-26 20:35:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>FastPet</title>
+	<link rel="stylesheet" href="principal.css">
+	<div class="header">
+	  <a href="#default" class="logo"><img src="images\PetFast.png" alt="logo" style="width:6%"></a>
+	  <div class="header-right">
+		<a href="#Login">Login</a>
+		<a href="#Register">Register</a>
+	  </div>
+	  <div class="header-center">
+        <a href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+	  </div>
+    </div>
+</head>
+<body style="background-color:#FFFFC0;">
+<div class="pagina">
+ <div class="icon">
+   <br><br><br><img src="images\mulher.jpg" alt="mulher" style="width:5%">
+   <h4>Joaninha65<h4>
+ </div>
+ <div class="imagem">
+  <img src="images\chorro.jpeg" alt="chorro" style="width:75%">
+  <h5>ArnaldoAnfitrias: Que fofooooo S2 S2 </h5>
+  <h5>SrCaio112: Onde você comprou essa coleirinha? </h5>
+  <h5>LitteTed: Serginho Cresceu kkkkk </h5>
+ </div>
+ <div class="icon">
+   <br><br><br><img src="images\crianca.png" alt="crianca" style="width:5%">
+   <h4>SrCaio112<h4>
+ </div>
+ <div class="imagem">
+  <img src="images\tinho.jpeg" alt="tinho" style="width:75%">
+  <h5>UrsinhoCaio: Bom Dia Caio</h5>
+  <h5>OElitista: Passou no veterinario que recomendei? </h5>
+  <h5>Juliana0908: Vou visitar vcs mais tarde </h5>
+ </div>
+ <div class="icon">
+   <br><br><br><img src="images\brother.png" alt="brother" style="width:7%">
+   <h4>PaiDePET<h4>
+ </div>
+ <div class="imagem">
+  <img src="images\pet.jpg" alt="pet" style="width:75%">
+  <h5>Joaninha65: Muito fofo como sempre </h5>
+  <h5>Jennifer2: Vou levar a Jennifer para visitar o Kaiser </h5>
+  <h5>JouiJouki: Essa foto foi tirada no CERET? </h5>
+ </div>
+</div>
+</body>
+</html>
